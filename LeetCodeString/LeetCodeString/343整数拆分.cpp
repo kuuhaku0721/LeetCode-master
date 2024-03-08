d65ac52275523be400e9cc09619f22e9ffcba78a
kuuhaku0721 <2674961592@qq.com>
2024-03-08 16:29:22 +0800

    343 整数拆分

---
+++ LeetCodeString/LeetCodeString/343整数拆分.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+/*
+* 给定一个正整数 n ，将其拆分为 k 个 正整数 的和（ k >= 2 ），并使这些整数的乘积最大化。
+* 返回 你可以获得的最大乘积 。
+* 
+* 这是动规 这是动规 这是动规....
+* 首先要确定dp数组
+* 拆分后每一位数都是这一位数所得乘积的最大值，也就是从dp[2]开始每一步都是最优的
+*/
+
+class Solution {
+public:
+	int integerBreak(int n) {
+		vector<int> dp(n + 1);
+		dp[2] = 1;
+		for (int i = 3; i <= n; i++) {
+			for (int j = 1; j <= i / 2; j++) {
+				dp[i] = max(dp[i], max((i - j) * j, dp[i - j] * j));
+			}
+		}
+		return dp[n];
+	}
+
+	// 还有个简单的做法，但没点能力想不出来那种（评论区人均大佬）
+	// 3生万物，不够的拆成2
+	int integerBreak1(int n) {
+		if (n <= 3) return n - 1;
+
+		switch (n % 3)
+		{
+		case 0:
+			return pow(3, n / 3);
+		case 1:
+			return pow(3, n / 3 - 1) * 4;  // 3 3 4 的乘积自然要比 3 3 3 1的乘积大
+		case 2:
+			return pow(3, n / 3) * 2;
+		}
+		return 0;
+	}
+};
+
+
+int main()
+{
+	cout << "hello world" << endl;
+	int n = 10;
+	int res = Solution().integerBreak1(n);
+	cout << res << endl;
+
+	system("pause");
+	return 0;
+}
+
---
+++ LeetCodeString/LeetCodeString/lq_2309_景区导游.cpp
@@ -0,0 +1,94 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+/*
+* 样例输入
+* 6 4
+* 1 2 1
+* 1 3 1
+* 3 4 2
+* 3 5 2
+* 4 6 3
+* 2 6 5 1
+* 
+* 样例输出
+* 10 7 13 14
+*/
+
+void TourGuide()
+{
+	int n, k;
+	cin >> n >> k;
+	vector<vector<int>> spot;
+	vector<int> path(k);
+	for (int i = 0; i < n - 1; i++)
+	{
+		vector<int> tmp(3);
+		for (int i = 0; i < 3; i++)
+		{
+			cin >> tmp[i];
+		}
+		spot.emplace_back(tmp);
+	}
+	for (int i = 0; i < k; i++)
+	{
+		cin >> path[i];
+	}
+
+	cout << "当前spot数组: " << endl;
+	for (auto vec : spot)
+	{
+		for (auto i : vec)
+		{
+			cout << i << " ";
+		}
+		cout << endl;
+	}
+	cout << "当前怕path数组：" << endl;
+	for (auto i : path)
+	{
+		cout << i << " ";
+	}
+	cout << endl;
+
+
+
+	vector<int> res;
+	// 当遍历的时候可以知道的是当前值和下一个值，刚好就是一站，然后到spot里面找是否有一个满足刚好有这俩数，有的话就加上[2]的距离，然后更新这俩数
+	// 然后继续寻找
+	for (int i = 0; i < path.size(); i++)
+	{
+		vector<int> currPath = path;
+		currPath.erase(currPath.begin() + i);
+		int len = 0;
+		for (int j = 0; j < currPath.size() - 1; j++)
+		{
+			// 我需要拿到两个数，这个时候需要判断什么时候跳过
+			int beg = currPath[j], end = currPath[j + 1];
+			auto it = find_if(spot.begin(), spot.end(), [beg, end](vector<int>& vec) {
+				cout << "这次要查找的路径是: " << beg << "--" << end << endl;
+				return (vec[0] == beg && vec[1] == end) || (vec[1] == beg && vec[0] == end);
+				});
+			if(it != spot.end())
+				len += (*it)[2];
+		}
+		res.push_back(len);
+	}
+
+	for (auto i : res)
+	{
+		cout << i << " ";
+	}
+	cout << endl;
+}
+
+int main()
+{
+	TourGuide();
+
+	system("pause");
+	return 0;
+}
+
