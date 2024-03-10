f094670b93bd7c9fefa40706afb2b0121aabcad4
kuuhaku0721 <2674961592@qq.com>
2024-03-10 20:08:36 +0800

    lq_2310_砍树

---
+++ LeetCodeString/LeetCodeString/lq_2310_砍树.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+/*
+* 样例输入
+* 6 2
+* 1 2
+* 2 3
+* 4 3
+* 2 5
+* 6 5
+* 3 6
+* 4 5
+* 
+* 样例输出
+* 4
+* 
+*/
+
+/*
+* 哈哈哈草
+* 题都看不懂....
+* 很好，再一次确信我是sb
+*/
+
+int main()
+{
+	cout << "hello world" << endl;
+
+	system("pause");
+	return 0;
+}
+
