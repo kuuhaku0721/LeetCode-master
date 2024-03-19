2b3603bcbf750435199bf004f3a69616b07d8b04
kuuhaku0721 <2674961592@qq.com>
2024-03-19 18:44:22 +0800

    560 和为K的子数组

---
+++ LeetCodeString/LeetCodeString/438找到字符串中所有字母的异位词.cpp
@@ -0,0 +1,104 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <unordered_set>
+#include <unordered_map>
+#include <numeric>
+using namespace std;
+
+/*
+* 给定两个字符串 s 和 p，找到 s 中所有 p 的 异位词 的子串，返回这些子串的起始索引。不考虑答案输出的顺序。
+* 异位词 指由相同字母重排列形成的字符串（包括相同的字符串）。
+*/
+
+/*
+* 题目限定了长度，p的长度是固定的，所以就可以循环遍历了
+* KMP....倒是用不上，大概，除非超时
+* 考虑的写法是排序，要的是异位词，但没说必须顺序一致，都异位了，顺序肯定不用管吧
+* 很好，超时了，字符串超过2w的长度就超时
+*/
+
+class Solution1 {
+public:
+	vector<int> findAnagrams(string s, string p) {
+		vector<int> res;
+		if (s.size() < p.size()) return res;
+
+		unordered_map<char, int> umapp;
+		for (char c : p)
+			umapp[c]++;
+		int len = p.size();
+		for (int i = 0; i <= s.size() - len; i++)
+		{
+			string str = s.substr(i, len);
+			unordered_map<char, int> umap;
+			for (char c : str)
+				umap[c]++;
+			
+			bool flag = true;
+			for(char c : p)
+				if (umap[c] != umapp[c])
+				{
+					flag = false;
+					break;
+				}
+			if (flag)
+				res.push_back(i);
+		}
+		return res;
+	}
+};
+
+/*
+* 一个不会超时的答案
+*/
+class Solution {
+public:
+	vector<int> findAnagrams(string s, string p)
+	{
+		int slen = s.size(), plen = p.size(), sum = 0, init = 0;
+		if (slen < plen)
+			return vector<int>();
+
+		char ch;
+		for (int i = 0; i < plen; i++)
+		{
+			ch = s[i];
+			sum = (sum + (ch * (ch - 18) * (ch - 'a'))) % 1584952641;
+			ch = p[i];
+			init = (init + (ch * (ch - 18) * (ch - 'a'))) % 1584952641;
+		}
+
+		vector<int> ans;
+		for (int i = plen; i <= slen; i++)
+		{
+			if (init == sum)
+				ans.push_back(i - plen);
+
+			ch = s[i - plen];
+			sum = sum - (ch * (ch - 18) * (ch - 'a'));
+			ch = s[i];
+			sum = (sum + (ch * (ch - 18) * (ch - 'a'))) % 1584952641;
+		}
+		return ans;
+	}
+};
+
+int main()
+{
+	cout << "hello world" << endl;
+	string s = "cbaebabacd";
+	string q = "abc";
+	auto vec = Solution().findAnagrams(s, q);
+	for (auto i : vec)
+	{
+		cout << i << " ";
+	}
+	cout << endl;
+
+
+	system("pause");
+	return 0;
+}
+
---
+++ LeetCodeString/LeetCodeString/560和为K的子数组.cpp
@@ -0,0 +1,80 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+/*
+* 给你一个整数数组 nums 和一个整数 k ，请你统计并返回 该数组中和为 k 的子数组的个数 。
+* 子数组是数组中元素的连续非空序列。
+*/
+
+/*
+* 注意连续，非空，连续，连续
+* 感觉是个滑动窗口的题，但是分类分到了子串，不知道为啥
+* 看来双指针是不行了，它有负数.......你不要等我错完了再告诉我啊....
+* 暴力双循环，超时...
+* 学到个新东西，一个叫前缀和的东东
+* 好家伙，这题是除了前缀和就没解法了吗，全都是前缀和
+*/
+
+class Solution {
+public:
+	int subarraySum1(vector<int>& nums, int k) {
+		if (nums.empty()) return 0;
+		if (nums.size() < 2) return nums[0] == k ? 1 : 0;
+
+		int sum = 0;
+		int wndSize = 1;
+		int res = 0;
+		while (wndSize <= nums.size())
+		{
+			for (int i = 0; i <= nums.size() - wndSize; i++)
+			{
+				int sum = 0;
+				for (int j = i; j < i + wndSize; j++)
+				{
+					sum += nums[j];
+				}
+				if (sum == k) res++;
+			}
+			wndSize++;
+		}
+		return res;
+	}
+	int subarraySum(vector<int>& nums, int k) {
+		int res = 0;
+		// 前缀和好理解，只不过多一个第一个是0，不然后面没法判断
+		vector<int> prefixsum(nums.size() + 1, 0);
+		for (int i = 0; i < nums.size(); i++)
+		{
+			prefixsum[i + 1] = prefixsum[i] + nums[i];
+		}
+		// 然后是这个哈希表是如何判断的，哈希表判断(不管find还是count都一样，找存不存在)是拿当前位置的前缀和减去目标数
+		// 如果减的结果等于0，说明这个位置的前缀和刚好就是目标结果，也就是从0开始一直加到当前位置，总和刚好就是结果
+		// 如果减的结果不等于0，且在哈希表中存在，说明当前位置的前缀和（pre1）减去目标数之后的结果是另一个前缀和(pre2)
+		// ，也就代表着从那个前缀和(pre2)的位置开始加，一直加到当前位置(pre1的位置)，总和刚好就是结果
+		// 如果减的结果不等于0还在哈希表中没有，说明不存在某一段的总和结果能够等于结果
+		unordered_map<int, int> umap;
+		for (int i = 0; i < prefixsum.size(); i++)
+		{
+			if (umap.count(prefixsum[i] - k) != 0)
+				res += umap[prefixsum[i] - k];
+			umap[prefixsum[i]]++;
+		}
+	}
+};
+
+
+int main()
+{
+	cout << "hello world" << endl;
+	vector<int> nums{ 1, -1, 0 };
+	int k = 0;
+	int res = Solution().subarraySum(nums, k);
+	cout << res << endl;
+
+	system("pause");
+	return 0;
+}
+
