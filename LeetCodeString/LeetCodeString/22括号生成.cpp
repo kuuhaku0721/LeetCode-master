c681654c402a09ecca7953e9f18a286d485da617
kuuhaku0721 <2674961592@qq.com>
2024-03-24 20:13:55 +0800

    79 单词搜索

---
+++ LeetCodeString/LeetCodeString/22括号生成.cpp
@@ -0,0 +1,159 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <stack>
+#include <functional>
+using namespace std;
+
+/*
+* 数字 n 代表生成括号的对数，请你设计一个函数，用于能够生成所有可能的并且 有效的 括号组合。
+*/
+
+/*
+* 起码，一眼看出来，应该是个回溯
+* 穷举好说，但是怎么判断是个合法的括号？
+* 一个个判断吗？
+* 那，绝对会超时的吧...
+* 特么的做不出来了....
+*/
+class Solution1 {
+public:
+	vector<string> result;
+	string str;
+	vector<char> thesis;
+	vector<string> generateParenthesis(int n) {
+		for (int i = 0; i < n; i++)
+		{
+			thesis.push_back('(');
+			thesis.push_back(')');
+		}
+		BackTracking(n * 2, -1);
+		return result;
+	}
+
+	void BackTracking(int len, int index)
+	{
+		if (str.size() == len)
+		{
+			cout << "当次的str = " << str << endl;
+			if (str[0] == ')') return;
+			if (isValid(str))
+				result.push_back(str);
+			return;
+		}
+
+		// 从左或者右括号里面取一个，然后继续回溯
+		for (int i = index + 1; i < len; i++)
+		{
+			str += thesis[i];
+			BackTracking(len, i);
+			str.pop_back();
+		}
+	}
+
+	bool isValid(string s)
+	{
+		if (s[0] == ')') return false;
+		stack<char> stk;
+		for (int i = 0; i < s.size(); i++)
+		{
+			if (s[i] == '(') stk.push(s[i]);
+			else
+			{
+				if (!stk.empty())
+				{
+					char c = stk.top();
+					if (c == '(')
+						stk.pop();
+				}
+				else
+					return false;
+			}
+
+		}
+		if (stk.empty())
+			return true;
+		else
+			return false;
+	}
+};
+
+/*
+* 这个方法也不错
+*/
+class Solution2 
+{
+	void backtrack(vector<string>& ans, string& cur, int open, int close, int n) {
+		if (cur.size() == n * 2) {
+			ans.push_back(cur);
+			return;
+		}
+
+		if (open < n) {
+			cur.push_back('(');
+			backtrack(ans, cur, open + 1, close, n);
+			cur.pop_back();
+		}
+		if (close < open) {
+			cur.push_back(')');
+			backtrack(ans, cur, open, close + 1, n);
+			cur.pop_back();
+		}
+	}
+public:
+	vector<string> generateParenthesis(int n) {
+		vector<string> result;
+		string current;
+		backtrack(result, current, 0, 0, n);
+		return result;
+	}
+};
+
+class Solution {
+public:
+	int m{};
+	vector<string> ans;
+	string path;
+	vector<string> generateParenthesis(int n) {
+		m = n * 2;
+		string t(m, 0);
+		path = t;
+
+		dfs(0, 0);
+		return ans;
+	}
+
+	void dfs(int i, int open)
+	{
+		if (i == m) 
+		{	// 括号构造完毕
+			ans.emplace_back(path); // 加入答案
+			return;
+		}
+
+
+		if (open < m / 2) 
+		{	// 可以填左括号
+			path[i] = '(';			// 直接覆盖
+			dfs(i + 1, open + 1);	// 多了一个左括号
+		}
+		if (i - open < open) 
+		{	// 可以填右括号
+			path[i] = ')';			// 直接覆盖
+			dfs(i + 1, open);
+		}
+	}
+};
+
+int main()
+{
+	cout << "hello world" << endl;
+	auto res = Solution().generateParenthesis(3);
+	for (auto s : res)
+		cout << s << endl;
+
+	system("pause");
+	return 0;
+}
+
---
+++ LeetCodeString/LeetCodeString/79单词搜索.cpp
@@ -0,0 +1,89 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+/*
+* 给定一个 m x n 二维字符网格 board 和一个字符串单词 word 。如果 word 存在于网格中，返回 true ；否则，返回 false 。
+* 单词必须按照字母顺序，通过相邻的单元格内的字母构成，其中“相邻”单元格是那些水平相邻或垂直相邻的单元格。同一个单元格内的字母不允许被重复使用。
+*/
+
+/*
+* 笑死，难得一见的问题
+* backTracking的函数，最后一个参数，如果用引用&，会出错，因为上下左右如果用的同一个数组会影响到其他位置的判断
+* 但是如果不用引用，会出现：超出内存限制...233333
+* 放弃，cv吧
+*/
+class Solution {
+public:
+	string word;
+	bool exist(vector<vector<char>>& board, string word) {
+		this->word = word;
+		bool flag = false;
+		vector<vector<char>> backup;
+		for (int i = 0; i < board.size(); i++)
+		{
+			for (int j = 0; j < board[i].size(); j++)
+			{
+				if (board[i][j] == word[0])
+				{
+					backup = board;
+					flag |= backTracking(0, i, j, backup);
+				}
+			}
+		}
+		return flag;
+	}
+
+	bool backTracking(int pos, int row, int col, vector<vector<char>> board)
+	{
+		if (pos == word.size() - 1)
+		{
+			if (word[pos] == board[row][col])
+				return true;
+			else
+				return false;
+		}
+
+		bool flag_up = false, flag_down = false, flag_left = false, flag_right = false;
+		if (board[row][col] == word[pos])
+		{
+			board[row][col] = '0';	// 用完之后置空 不然会被重复利用出错
+			if (row - 1 >= 0)
+			{
+				flag_up = backTracking(pos + 1, row - 1, col, board);
+			}
+			if (row + 1 < board.size())
+			{
+				flag_down = backTracking(pos + 1, row + 1, col, board);
+			}
+			if (col - 1 >= 0)
+			{
+				flag_left = backTracking(pos + 1, row, col - 1, board);
+			}
+			if (col + 1 < board[row].size())
+			{
+				flag_right = backTracking(pos + 1, row, col + 1, board);
+			}
+		}
+
+		return flag_up || flag_down || flag_left || flag_right;
+	}
+};
+
+
+int main()
+{
+	cout << "hello world" << endl;
+	vector<vector<char>> board {
+		{'A', 'B', 'C', 'E'},
+		{'S', 'F', 'E', 'S'},
+		{'A', 'D', 'E', 'E'}
+	};
+	string word = "ABCESEEEFS";
+	cout << Solution().exist(board, word) << endl;
+
+	system("pause");
+	return 0;
+}
+
