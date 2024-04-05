4e0734edf2d7ecbf21b69f5c91e22876d5d938e9
kuuhaku0721 <2674961592@qq.com>
2024-04-05 18:45:25 +0800

    152 乘积最大子数组

---
+++ LeetCodeString/LeetCodeString/152乘积最大子数组.cpp
@@ -0,0 +1,68 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+/*
+* 给你一个整数数组 nums ，请你找出数组中乘积最大的非空连续子数组
+* （该子数组中至少包含一个数字），并返回该子数组所对应的乘积。
+* 测试用例的答案是一个 32-位 整数。
+*/
+
+/*
+* 单层dp思路还是容易想出来的
+* dp[i]就是乘到它的时候的乘积
+* 只不过这个乘积要判断，如果跟他前面那个相比，更大了，则更新为这个大值 dp[i] = nums[i] * dp[i - 1]
+* 如果变小了，那就不变化，dp[i] = nums[i]
+*/
+
+class Solution1 {
+public:
+	int maxProduct(vector<int>& nums) {
+		vector<int> dp(nums.size());
+		dp[0] = nums[0];
+		for (int i = 1; i < nums.size(); i++)
+		{
+			int curr = dp[i - 1] * nums[i];
+			// 除了判断乘积，还得判断一下和当前值相比哪个大
+			// 也就是现在要比对的是三个值：curr乘积，dp[i - 1]上一个乘积，nums[i]当前值
+			if (curr < dp[i - 1])
+				dp[i] = nums[i];
+			else
+				dp[i] = max({ curr, dp[i - 1], nums[i] });
+
+		}
+		return *max_element(dp.begin(), dp.end());
+	}
+};
+
+// 来看官方题解
+// 一句话概括就是：因为有负数的出现会让最小变最大，最大变最小，所以在维护最大值dp的时候也要维护一个最小值dp
+// 评论区的大佬们的一个比较精明的写法是先遍历一遍找最大，再反过来一遍找最小
+
+class Solution {
+public:
+	int maxProduct(vector<int>& nums) {
+		vector <int> maxF(nums), minF(nums);
+		for (int i = 1; i < nums.size(); ++i) {
+			maxF[i] = max(maxF[i - 1] * nums[i], max(nums[i], minF[i - 1] * nums[i]));
+			minF[i] = min(minF[i - 1] * nums[i], min(nums[i], maxF[i - 1] * nums[i]));
+		}
+		return *max_element(maxF.begin(), maxF.end());
+	}
+};
+
+
+int main()
+{
+	cout << "hello world" << endl;
+	// vector<int> nums{ 0,2 };
+	// vector<int> nums{ 2,3,-2,4 };
+	vector<int> nums{ -2,3,-4 };		// 卡在这个用例过不去了
+	int res = Solution().maxProduct(nums);
+	cout << res << endl;
+
+	system("pause");
+	return 0;
+}
+
---
+++ LeetCodeString/LeetCodeString/300最长递增子序列.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+/*
+* 给你一个整数数组 nums ，找到其中最长严格递增子序列的长度。
+* 子序列 是由数组派生而来的序列，删除（或不删除）数组中的元素而不改变其余元素的顺序。例如，[3,6,2,7] 是数组 [0,3,1,6,2,2,7] 的
+* 子序列		不用连续，递增即可
+*/
+
+/*
+* 有个思路，dp[i]就是nums[i]的前面比它小的数的个数
+* 计算dp[i]需要找到它前面第一个比它小的数，在它基础上加1
+* 
+* 好欸，第一次做动规题，能够自己想出来自己做出来还能有个不错的效率
+*/
+
+class Solution {
+public:
+	int lengthOfLIS(vector<int>& nums) {
+		vector<int> dp(nums.size(), 0);
+		dp[0] = 0;
+		for (int i = 1; i < nums.size(); i++)
+		{
+			int j = i - 1;
+			int big = 0;
+			bool flag = false; // 是否有比它小的存在的标志
+			for (; j >= 0; --j)
+			{	// 不能是第一个，得是最大那一个
+				if (nums[i] > nums[j])
+				{
+					big = max(big, dp[j]);
+					flag = true;
+				}
+			}
+			if (flag) dp[i] = big + 1;	// 只有上面的if判断过，才说明有小于它的，需要增加
+		}
+		return (*max_element(dp.begin(), dp.end())) + 1;  // 最后+1是因为前面算的时候没算自己
+	}
+};
+
+int main()
+{
+	cout << "hello world" << endl;
+	vector<int> nums{ 10,9,2,5,3,7,101,18 };
+	int res = Solution().lengthOfLIS(nums);
+	cout << res << endl;
+
+	system("pause");
+	return 0;
+}
+
