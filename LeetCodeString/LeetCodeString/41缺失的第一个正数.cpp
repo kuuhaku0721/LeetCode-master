b64472071b9e4475060a21996cf1f24f9ae554bf
kuuhaku0721 <2674961592@qq.com>
2024-04-30 10:46:13 +0800

    41 缺失的第一个正数

--- LeetCodeString/LeetCodeString/41缺失的第一个正数.cpp
+++ LeetCodeString/LeetCodeString/41缺失的第一个正数.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <algorithm>
+#include <unordered_set>
 using namespace std;
 
 /*
@@ -8,10 +10,26 @@ using namespace std;
 * 请你实现时间复杂度为 O(n) 并且只使用常数级别额外空间的解决方案。
 */
 
+
+/*
+* 按题目这个说法，我直接排序不就好了吗？排序，然后从第一个正数开始累加，只要断开，那就是它，直接返回
+* 好了，现在问题变成了，怎么找到这个返回值，要求的是最小的返回值，问题是，这个最小，是从1开始算的..
+* 
+* 笑死，0.13% 
+* 但不管怎么说，你就说做没做出来吧
+*/
 class Solution {
 public:
 	int firstMissingPositive(vector<int>& nums) {
-
+		unordered_set<int> uset(nums.begin(), nums.end());
+		int maxEle = *max_element(nums.begin(), nums.end());
+		for (int i = 1; i <= maxEle; i++)
+		{
+			if (uset.find(i) == uset.end())
+				return i;
+		}
+		if (maxEle <= 0) return 1;
+		else return maxEle + 1;
 	}
 };
 
